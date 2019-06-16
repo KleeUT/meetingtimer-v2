ed4111122bc9f552e66d1a88d4ab50810ab4593c
Kleeut <klee.ut@gmail.com>
2019-06-16 17:53:08 +1000

    :sparkle: hide controls when they're not needed

--- src/controls/TimerControls.tsx
+++ src/controls/TimerControls.tsx
@@ -1,14 +1,44 @@
 import React from "react";
 import { buttons } from "../components";
 import { useActionCreator } from "./controlsActionCreator";
+import { useGlobalState } from "../GlobalState";
+const { PrimaryButton, WarningButton, SecondaryButton } = buttons;
+
 export default () => {
+  const { state } = useGlobalState();
   const { startTimer, stopTimer, resetTimer } = useActionCreator();
-  const { PrimaryButton, WarningButton, SecondaryButton } = buttons;
   return (
     <div>
-      <SecondaryButton onClick={stopTimer}>Stop</SecondaryButton>
-      <PrimaryButton onClick={startTimer}>Start</PrimaryButton>
-      <WarningButton onClick={resetTimer}>Reset</WarningButton>
+      <StartButton isRunning={state.isRunning} action={startTimer} />
+      <StopButton isRunning={state.isRunning} action={stopTimer} />
+      <ResetButton isRunning={state.isRunning} action={resetTimer} />
     </div>
   );
 };
+
+const StopButton = ({
+  isRunning,
+  action
+}: {
+  action: () => void;
+  isRunning: boolean;
+}) =>
+  isRunning ? <SecondaryButton onClick={action}>Stop</SecondaryButton> : null;
+
+const ResetButton = ({
+  isRunning,
+  action
+}: {
+  action: () => void;
+  isRunning: boolean;
+}) =>
+  isRunning ? <WarningButton onClick={action}>Reset</WarningButton> : null;
+
+const StartButton = ({
+  isRunning,
+  action
+}: {
+  action: () => void;
+  isRunning: boolean;
+}) =>
+  isRunning ? null : <PrimaryButton onClick={action}>Start</PrimaryButton>;
